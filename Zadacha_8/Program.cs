ce2d3050cbe70a53f1495c109d38c0fb94ba48d3
Вячеслав <vyacheslav.ivanov.r38@mail.ru>
2022-08-11 15:16:11 +0900

    Программа, показывает все четные числа от 1 до N

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Программа, показывает все четные числа от 1 до N:");
+Console.Write("Введите число:  ");
+string? numberString = Console.ReadLine();
+int N = int.Parse(numberString);
+
+int index = 1;
+
+   while(index < N)   
+    {
+    index++;
+    Console.Write("Четные числа в иентервале от 1 до N:  ");
+    Console.WriteLine(index++ );
+    }
