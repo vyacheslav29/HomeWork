d75c2c066da4854c5243af26dd66f44b976eba9b
Вячеслав <vyacheslav.ivanov.r38@mail.ru>
2022-08-13 20:39:27 +0900

    Программа, выводит третью цифру заданного числа на экран

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Программа, выводит третью цифру заданного числа на экран: ");
+Console.Write("Введите число:  ");
+string? numberString = Console.ReadLine();
+int Number = int.Parse(numberString!);
+
+while(Number > 1000) 
+{
+Number = Number / 10;
+}
+
+if(Number > 99)
+{
+    int Digit3 = Number % 10;
+    int result = Digit3;
+    Console.WriteLine("Третьей цифрой заданного Вами числа, является:  " + result);
+} 
+
+if(Number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет!  ");
+}
